836619af7ac9898d5942f333b053dc92fa811bf8
AYSung <aysung300@gmail.com>
2015-01-13 00:28:56 -0500

    added check for valid input (only ACGT)

--- app.js
+++ app.js
@@ -9,11 +9,48 @@
         $scope.convertInput = function(input){
 
 
+            input = formatInput(input);
 
+            if (checkBases(input)){
+                input = replaceBases(input);
 
-            
-            $scope.output = input.toUpperCase();
-        }
+                $scope.output = input;
+            } else {
+                alert("Invalid sequence");
+            }
+        };
+
+        var replaceBases = function(input){
+            var count = 0;
+            while(input.indexOf('U') != -1){
+                input = input.replace('U', 'T');
+                count += 1;
+            }
+            if (count > 0){
+                alert("U replaced with T " + count + " times");
+            }
+            return input;
+        };
+
+        var checkBases = function(input){
+
+            while(input.length > 0){
+                if (input.charAt(0) == 'A' || input.charAt(0) == 'C' || input.charAt(0) == 'G' ||
+                        input.charAt(0) == 'T' || input.charAt(0) == 'U'){
+                    input = input.substring(1, input.length);
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        var formatInput = function(input){
+            input = input.trim();
+            input = input.replace(' ', '');
+            input = input.toUpperCase();
+            return input;
+        };
 
     }]);
 
